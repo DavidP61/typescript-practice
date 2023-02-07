6d7ab591b1e2967b0532b89e02ee813ec50f3bc5
DavidP61 <dme6036@naver.com>
2023-02-08 01:07:25 +0900

    Make form
    Use TypeScript & React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="black" />
-      <Circle bgColor="tomato" text="not default" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
