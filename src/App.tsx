b8c3707791a4918665e7a2da0c69fc44ad7d8028
DavidP61 <dme6036@naver.com>
2023-02-08 17:46:18 +0900

    upgrade readability

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState("");
+  
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
     setValue(value);
   };
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Hello", value);
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
